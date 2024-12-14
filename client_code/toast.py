dc7b168d1eaf6b92279aac14a0f2996947732ea8
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2024-12-14 05:24:30 +0000

    Created styles_data, Toaster and toast; Edited settings; Deleted Form1

---
+++ client_code/toast.py
@@ -0,0 +1,11 @@
+from anvil import *
+from .Toaster import Toaster
+from anvil.js.window import document
+from . import styles_data
+
+class Toast:
+    def init(self, message, style="info", timeout = 2, title=""): #Title is added just to migrate from Notifications and does not actually show
+        toaster = Toaster()
+        To
+        toaster
+        
