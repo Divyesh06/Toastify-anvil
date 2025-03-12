6a065b6e4c42fe3b004608e0741ed64689706980
divyeshlakhotia@gmail.com <divyeshlakhotia@gmail.com>
2025-03-12 06:31:11 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -6,7 +6,7 @@ class Form1(Form1Template):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
+        
         # Any code you write here will run before the form opens.
 
     def button_1_click(self, **event_args):
